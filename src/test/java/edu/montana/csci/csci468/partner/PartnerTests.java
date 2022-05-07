79f43eebf1baf4e2044ec49548be0ccbd80d8166
Rory McLean <54718289+RoryDMcLean@users.noreply.github.com>
2022-05-07 01:41:14 -0600

    Implemented PartnerTests

--- src/test/java/edu/montana/csci/csci468/partner/PartnerTests.java
+++ src/test/java/edu/montana/csci/csci468/partner/PartnerTests.java
@@ -1,64 +1,38 @@
 /*
-* Author: Rory McLean
-* Last Date Modified: 5/1/2022
-* Test file for partner for Compiler Capstone project.
-* Descriptions of the tests are listed below.
-*/
+This File was written by Mason Medina for Rory McLean to use in the Catscript Compiler
+ */
 package edu.montana.csci.csci468.partner;
-
 import edu.montana.csci.csci468.CatscriptTestBase;
-import edu.montana.csci.csci468.parser.expressions.*;
+import edu.montana.csci.csci468.parser.ParseErrorException;
 import org.junit.jupiter.api.Test;
 
-import static edu.montana.csci.csci468.tokenizer.TokenType.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class PartnerTests extends CatscriptTestBase{
+public class PartnerTests extends CatscriptTestBase {
+
+
+
     @Test
-    public void symbolsAndCommentsTokenizeTogether(){
-        //This test verifies that symbols and comments are tokenized all in the same set of code
-        assertTokensAre("9 + 10 = 21\n" +
-                        "// no its not",
-                INTEGER, PLUS, INTEGER, EQUAL, INTEGER, SLASH, SLASH, EOF);
+    void scopingWorksProperly() {
+        assertThrows(ParseErrorException.class, () -> {
+            executeProgram("var x = false function func() {var x : int = 1}");
+        });
     }
 
     @Test
-    public void largeExpressionIsHandledCorrectly(){
-        //This test verifies that a large expression is recognized and evaluated correctly
-        String strExpr = "(36 * (81 / 9)) + ((52 / 13) - (39 * 69))";
-        AdditiveExpression expr = parseExpression(strExpr);
-        assertTrue(expr.isAdd());
-        assertTrue(((ParenthesizedExpression)expr.getLeftHandSide()).getExpression() instanceof FactorExpression);
-        assertTrue(((ParenthesizedExpression)expr.getRightHandSide()).getExpression() instanceof AdditiveExpression);
+    void variableAssignment() {
+        String  testString = "var x = 0\n" +
+                "print(x)\n" +
+                "x = 1\n" +
+                "print(x)";
 
-        //This set of code verifies the left side of the larger expressions
-        FactorExpression exprLeft =
-                (FactorExpression) ((ParenthesizedExpression)expr.getLeftHandSide()).getExpression();
-        assertTrue(exprLeft.getLeftHandSide() instanceof IntegerLiteralExpression);
-        assertTrue(((ParenthesizedExpression) exprLeft.getRightHandSide()).getExpression() instanceof FactorExpression);
-        assertEquals(((IntegerLiteralExpression) exprLeft.getLeftHandSide()).getValue(), 36);
-
-        //This set of code verifies the right side of the larger expressions
-        AdditiveExpression exprRight =
-                (AdditiveExpression) ((ParenthesizedExpression)expr.getRightHandSide()).getExpression();
-        assertTrue(((ParenthesizedExpression) exprRight.getLeftHandSide()).getExpression()
-                instanceof FactorExpression);
-        assertTrue(((ParenthesizedExpression) exprRight.getRightHandSide()).getExpression()
-                instanceof  FactorExpression);
-        assertEquals(((IntegerLiteralExpression) ((FactorExpression) ((ParenthesizedExpression)
-                exprRight.getLeftHandSide()).getExpression()).getRightHandSide()).getValue(), 13);
-
-        //This line of code tests if the expression is evaluated properly
-        assertEquals(evaluateExpression(strExpr), -2363);
+        assertEquals("0\n1\n", executeProgram(testString));
     }
 
     @Test
-    public void forStatementsIterateThroughDefinedLists(){
-        /* This test verifies that a segment of code is able to properly execute, specifically testing
-        * list variables and for loops.
-        * */
-        String forStmt = "var newList : list<int> = [1, 2, 3, 4, 5]\n" +
-                "for(x in newList) { print(x * x) }";
-        assertEquals("1\n4\n9\n16\n25\n", executeProgram(forStmt));
+    void IfFunctionWorksProperly() {
+        assertEquals("false\n", executeProgram("function foo(x : int) : bool { if (x>0) {return true}" +
+                "else {return false}} print(foo(0))"));
     }
+
 }
